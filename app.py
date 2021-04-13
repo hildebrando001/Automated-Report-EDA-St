81a9de12c4b8fcbdaf2e54ad1e35c744ca1f319e
José Hildebrando <jose.hildebrando@gmail.com>
2021-04-13 16:14:42 -0300

    update menu

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import streamlit as st
-
+import pandas as pd
+import codecs # Helps to load file
+import streamlit.components.v1 as components # version 1
 
 def main():
     """Exploratory Data Analysis(EDA) Application with Streamlit Component
     """
     menu = ["Home", "Pandas Profile", "Sweetviz", "About"]
-    choice = st.sidebar.select("Menu", menu)
+    choice = st.sidebar.selectbox("Menu", menu)
 
     if choice == "Pandas Profile":
         st.subheader("Automated EDA with Pandas")
@@ -18,7 +20,10 @@ def main():
 
     else:
         st.subheader("Home")
+        components.html("<p style='color:red;'> Streamlit components is awosome. </p>")
 
 
-if __main__ == '__main__':
+# It makes possible to know if a script is coming from an importation or 
+# if it's coming from of the principal application
+if __name__ == '__main__':
     main()
